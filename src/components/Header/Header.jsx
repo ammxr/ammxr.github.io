d79aef877f71014b10ee58a1ee3dbe9f6bd2d7c7
Ammar Hakim <ammarhakim57@gmail.com>
2023-05-31 14:05:52 -0400

    Header Component
    Started Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="h-container">
+      <div className="header">
+        <h-title>Ammar<br/>Hakim</h-title>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
