fb43061223db60a41f53fe2304dd3a0f34dcb5cd
Ammar Hakim <ammarhakim57@gmail.com>
2023-06-15 20:40:59 -0400

    Update App

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -10,11 +10,11 @@ const AboutMe = () => {
 
       <div className="a-left">
         <div className="a-contents">
-          <span>About Me</span>
-          <span>
-            Who is Ammar Hakim?  I am a 17-year-old boy born in Gujrat Pakistan coming to this country when I was just 2 years old. Upon immigration, my family and I had settled in an apartment in Toronto ON for a period of 5 years where we had then moved to a house in Mississauga where we have permanently settled ever since.
-            My Hobbies Personally, I come to enjoy lots of various activities, my favourite, team sports. Some hobbies which I enjoy include sports such as basketball and soccer which in regular years have played outside of school leagues. In terms of my enjoyment solo, I love playing video games with cool stories and journeys and very much enjoy programming. Programming is something which I have day by day been taking excessive interest in-- the language of machines. I am also keen in the field of video editing and photoshop. Over the course of many years I have become very proficient and strong at these skills and day by day continue to develop and practice as a sort of art distracting my mind from stressors.           
-          </span>
+          <h1 className="a-title">About Me</h1>
+          <p className="a-description">
+            Who is Ammar Hakim? I am a 17-year-old boy born in Gujrat Pakistan coming to this country when I was just 2 years old. Upon immigration, my family and I had settled in an apartment in Toronto ON for a period of 5 years where we had then moved to a house in Mississauga where we have permanently settled ever since.
+            My Hobbies Personally, I come to enjoy lots of various activities, my favourite, team sports. Some hobbies which I enjoy include sports such as basketball and soccer which in regular years have played outside of school leagues. In terms of my enjoyment solo, I love playing video games with cool stories and journeys and very much enjoy programming. Programming is something which I have day by day been taking excessive interest in-- the language of machines. I am also keen in the field of video editing and photoshop. Over the course of many years I have become very proficient and strong at these skills and day by day continue to develop and practice as a sort of art distracting my mind from stressors.
+          </p>
           <button className="button s-button">My Resume</button>
         </div>
       </div>
