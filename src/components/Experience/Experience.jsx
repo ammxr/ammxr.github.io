62f87e9207921fbb5e86d5e40564f4bbb131b35b
Ammar Hakim <ammarhakim57@gmail.com>
2023-06-16 15:33:47 -0400

    Experiences Complete

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,18 +3,18 @@ import './Experience.css';
 
 const Experience = () => {
   return (
-    <section className="e-sec">
+    <div className="e-sec">
 
         <div className="e-title">
-          <e-name>Experience</e-name>
+          <e-name>EXPERIENCE</e-name>
           <div className="e-wave"></div>
         </div>
 
         <div className="e-timeline">  
           <div className="e-experience">
-            <div className="e-year">2022</div>
+            <div className="e-year">2023</div>
             <div className="e-description">
-              <h2>Experience Title 1</h2>
+              <h2>Freelance Discord Chatbot Developer, Self Employed</h2>
               <ul>
                 <li>Create custom Discord Bots for moderation, utility, promotions and more</li>
                 <li>Python based combined with external APIs (weather) and database integrations</li>
@@ -25,11 +25,11 @@ const Experience = () => {
           </div>
 
           <div className="e-experience">
-            <div className="e-year">2021</div>
+            <div className="e-year">2022</div>
             <div className="e-description">
-              <h2>Experience Title 2</h2>
+              <h2>Programming Teacher, Taleem Academy</h2>
               <ul>
-                <li>Volunteered teaching weekly Python and JavaScript to youth ages (4-16) based off premade lessons/assignments</li>
+                <li>Volunteered teaching weekly Python and JS to youth ages (4-16) based off premade lessons</li>
                 <li>Self-made lessons, assignments, and testing for evaluation purposes</li>
                 <li>Wrote Midterm + Final report cards alongside grading schemes</li>
                 <li> Helped organize school events (For holidays, significant days etc)</li>
@@ -38,9 +38,9 @@ const Experience = () => {
           </div>
 
           <div className="e-experience">
-            <div className="e-year">2021</div>
+            <div className="e-year">2022</div>
             <div className="e-description">
-              <h2>Experience Title 2</h2>
+              <h2>Programming Teacher Assistant, Best Brains</h2>
               <ul>
                 <li>Served as an uncontracted Teacher Assistant helping youth ages 5-12 Python and Scratch courses.</li>
                 <li> Assigned Curriculum based lessons/assignments</li>
@@ -52,7 +52,7 @@ const Experience = () => {
           <div className="e-experience">
             <div className="e-year">2021</div>
             <div className="e-description">
-              <h2>Experience Title 2</h2>
+              <h2>Freelance Graphics Design, Self Employed</h2>
               <ul>
                 <li>Creating professional originals logo, Vectorizing premade Logos, Merchandise design/concepts</li>
                 <li>Material simulation (for realism) through After Effects</li>
@@ -64,7 +64,7 @@ const Experience = () => {
           <div className="e-experience">
             <div className="e-year">2021</div>
             <div className="e-description">
-              <h2>Experience Title 2</h2>
+              <h2>Store Associate, Dollarama</h2>
               <ul>
                 <li>Served primarily as Cashier or sorting in the Returns Department</li>
                 <li>Carts Disinfections, Sweeping, Front Facing Items, Stock Unboxing + Damaged Returns sorting</li>
@@ -74,7 +74,7 @@ const Experience = () => {
           </div>
 
       </div>
-  </section>
+  </div>
   );
 };
 
